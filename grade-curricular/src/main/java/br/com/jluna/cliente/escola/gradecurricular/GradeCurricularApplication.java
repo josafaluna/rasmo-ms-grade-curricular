abafbefe3765efc8ba031f43aed2db9a2dd44b7e
Josafa Luna <josafaluna@gmail.com>
2021-02-21 12:22:16 -0300

    Expondo nosso serviço na Controller

--- grade-curricular/src/main/java/br/com/jluna/cliente/escola/gradecurricular/GradeCurricularApplication.java
+++ grade-curricular/src/main/java/br/com/jluna/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -2,9 +2,22 @@ package br.com.jluna.cliente.escola.gradecurricular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author jluna SpringBootServletInitializer Nosso projeto trata-se de um
+ *         “servlet”, ou seja, tem a funcionalidade de um servidor.
+ *
+ */
 
 @SpringBootApplication
-public class GradeCurricularApplication {
+public class GradeCurricularApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GradeCurricularApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeCurricularApplication.class, args);
---
+++ grade-curricular/src/main/java/br/com/jluna/cliente/escola/gradecurricular/controllers/MateriaController.java
@@ -0,0 +1,18 @@
+package br.com.jluna.cliente.escola.gradecurricular.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/materia")
+public class MateriaController {
+
+	@GetMapping
+	public ResponseEntity<String> helloRest() {
+		return ResponseEntity.status(HttpStatus.OK).body("Curso - Spring Boot - Backend");
+	}
+
+}
